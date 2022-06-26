3819cc30f38509fd5656c257dfa67a3a2d16f3c7
Ola-22 <olanazly@gmail.com>
2022-06-26 14:29:24 +0300

    Add files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">d</div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
