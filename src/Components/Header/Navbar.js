c87f4bb13df21109874879e857a153cd1705540b
Ola-22 <olanazly@gmail.com>
2022-07-17 09:44:19 +0300

    Add files

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -1,27 +1,28 @@
 import * as S from "./style";
+import { Link } from "react-router-dom";
 
 function Navbar({ open }) {
   return (
     // <div>
     <S.Ul open={open}>
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
 
       <li>
-        <a href="/">Who we are</a>
+        <a href="#who-we-are">Who we are</a>
       </li>
 
       <li>
-        <a href="/">Features</a>
+        <a href="#features">Features</a>
       </li>
 
       <li>
-        <a href="/">Testimonials</a>
+        <a href="#testimonials">Testimonials</a>
       </li>
 
       <li>
-        <a href="/">Contact Us</a>
+        <a href="#contact-us">Contact Us</a>
       </li>
       {/* <button>download app</button> */}
     </S.Ul>
